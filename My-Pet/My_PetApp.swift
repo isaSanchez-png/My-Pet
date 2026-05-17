929896e6f45d7b899b72c605241cdab308148129
isaSanchez-png <isabela.sanchez.mol@gmail.com>
2026-05-17 11:52:14 -0500

    Initial Commit

---
+++ My-Pet/My_PetApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_PetApp.swift
+//  My-Pet
+//
+//  Created by Isa on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct My_PetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
